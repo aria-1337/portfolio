ddcc8361c7236fc6946485f0b0ada933ca32b0e1
aria-1337 <aria.lopez.dev@gmail.com>
2024-03-18 23:38:57 -0700

    Added name title and personal statement section

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import LeftContent from './components/LeftContent';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Left>
+            <LeftContent />
+        </Left>
+
+        <Right>
+        </Right>
+    </Container>
   );
 }
 
-export default App;
+// Container Hold two main columns
+// [LEFT] = Name/Title/General Info
+// [RIGHT] = Scrolling content
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 0;
+    margin: 0;
+`;
+
+const Left = styled.div`
+    width: 50%;
+`;
+
+const Right = styled.div`
+    width: 50%;
+`;
+
---
+++ src/components/LeftContent.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function LeftContent() {
+    return (
+        <Container>
+            <InfoBlock>
+                <Name>Aria Lopez</Name>
+                <Title>Fullstack Engineer</Title>
+                <Statement>I believe in mission driven companies, collaborative teams, and products that make our ends users happy.</Statement>
+            </InfoBlock>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    padding: 80px 0px 0px 80px;
+    position: sticky;
+    top: 20px;
+`;
+
+const InfoBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Name = styled.h1`
+    font-size: 50px;
+    margin: 0;
+`;
+
+const Title = styled.h3`
+    margin: 5 0 0 5;;
+`;
+
+const Statement = styled.p`
+    margin: 0;
+    color: #7C8FB5;
+`;
